0a6f07ecbaec173174ea070bb1337edc592211ea
Naveen CS <catchspider2002@gmail.com>
2023-02-09 07:50:31 +0530

    Added russian lang files

--- src/routes/stores.js
+++ src/routes/stores.js
@@ -2,7 +2,7 @@ import { writable } from 'svelte/store';
 // import { onMount } from "svelte";
 
 export const themeStore = writable("light");
-export const langStore = writable("sv");
-export const webStore = writable("https://sv.pattern.monster");
+export const langStore = writable("ru");
+export const webStore = writable("https://ru.pattern.monster");
 
 // export let themeStore;
